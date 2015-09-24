68d38f78a32cfea3a6e38aa2f4782ae01f3b7306
Nazario A. Ayala <nazario@niztech.com>
2015-09-23 17:06:29 -0700

    have the basic SQL query in place

--- migrate_to_drupal.php
+++ migrate_to_drupal.php
@@ -1,4 +1,22 @@
 <?php
+require_once('settings.php');
+require_once('Medoo/medoo.php');
 
-$query = 'SELECT id, title, test, published, datecreated, shortdesc, doc, picture, custom2, custom3
-WHERE link IS NOT NULL';
+// $query = 'SELECT id, title, test, published, datecreated, shortdesc, doc, picture, custom2, custom3 WHERE link IS NOT NULL AND doc IS NULL AND ';
+
+$database = new Medoo($conSettings);
+$results = $database->debug()->select(
+  'articles',
+  ['id', 'title', 'test(body)', 'publish', 'datecreated', 'shortdesc', 'doc', 'picture', 'custom2', 'custom3'],
+  [ 'AND' => ['AND' => ['type[!]' => NULL, 'OR' => ['doc[!]' => NULL, 'body[!]' => NULL] ] ] ]
+);
+
+echo PHP_EOL;
+
+if($database->error()[0] != '00000'){
+  echo PHP_EOL;
+  echo '__Errors Found__';
+  echo PHP_EOL;
+  var_dump($database->error());
+  echo PHP_EOL;
+}
