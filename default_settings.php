5838c8b8ae8bc4275306996084c5d0f950241459
Nazario A. Ayala <nazario@niztech.com>
2015-09-24 00:40:51 -0700

    Added defualt settings

---
+++ default_settings.php
@@ -0,0 +1,13 @@
+<?php
+//Rename this file settings.php
+
+$output_filename = './output-double-double-quote-XX.csv';
+
+$conSettings = [
+  'database_type' => 'mysql',
+  'database_name' => 'db_name', //database name
+  'server' => '127.0.0.1', //database server
+  'username' => 'root', // database user
+  'password' => 'password', //database password
+  'charset' => 'utf8'
+];
