a5bb3a9c37be5baae87d4cfdacdc1d95e141c1b7
Nazario A. Ayala <nazario@niztech.com>
2015-09-23 17:55:28 -0700

    Finished 1 Will select 100 records, and changes datecreated to not bee a zero value, and adds slashes to data that may quotes.

--- migrate_to_drupal.php
+++ migrate_to_drupal.php
@@ -1,14 +1,15 @@
 <?php
+define('MODE', '!debug');
+
 require_once('settings.php');
 require_once('Medoo/medoo.php');
 
-// $query = 'SELECT id, title, test, published, datecreated, shortdesc, doc, picture, custom2, custom3 WHERE link IS NOT NULL AND doc IS NULL AND ';
-
 $database = new Medoo($conSettings);
-$results = $database->debug()->select(
+
+$results = $database->select(
   'articles',
-  ['id', 'title', 'test(body)', 'publish', 'datecreated', 'shortdesc', 'doc', 'picture', 'custom2', 'custom3'],
-  [ 'AND' => ['AND' => ['type[!]' => NULL, 'OR' => ['doc[!]' => NULL, 'body[!]' => NULL] ] ] ]
+  ['id', 'title', 'test(body)', 'publish', 'datecreated', 'shortdesc', 'doc', 'picture', 'custom2(amp_ref_id)', 'custom3(language)'],
+  [ 'OR' => ['AND' => ['link[=]' => NULL, 'OR' => ['doc[!]' => NULL, 'test[!]' => NULL] ] ], 'LIMIT' => 100 ]
 );
 
 echo PHP_EOL;
@@ -18,5 +19,30 @@ if($database->error()[0] != '00000'){
   echo '__Errors Found__';
   echo PHP_EOL;
   var_dump($database->error());
+  die();
   echo PHP_EOL;
+}
+
+
+$csv = '';
+$csv .= 'id, title, body, publish, datecreated, shortdesc, doc, picture, amp_ref_id, language' . "\n";
+
+foreach ($results as $key => &$result) {
+
+  if($result['datecreated'] == '0000-00-00 00:00:00'){
+    $result['datecreated'] = '1999-01-01 00:00:01';
+  }
+
+
+  $csv .= $result['id'] . ', ' . addslashes($result['title']) . ', ' . addslashes($result['body']) . ', ' . $result['publish'] . ', ' . $result['datecreated'] . ', ' . addslashes($result['shortdesc']) . ', ' . addslashes($result['doc']) . ', ' . addslashes($result['picture']) . ', ' . $result['amp_ref_id'] . ', ' . addslashes($result['language']) . "\n";
+
+  echo PHP_EOL;
+}
+
+if(MODE != 'debug'){
+  $file = fopen('./output.csv', 'w');
+  fwrite($file, $csv);
+  fclose($file);
+}else{
+  echo $csv;
 }
